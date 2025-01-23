5f5b549f0d6463870b78ab2e391cf95866b5cd9a
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2025-01-22 19:16:03 -0500

    change apollo to handle 2 endpoints

--- web/codegen.ts
+++ web/codegen.ts
@@ -5,9 +5,20 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   generates: {
-    "./src/gql/graphql.tsx": {
+    "./src/gql/indexer/graphql.tsx": {
       schema: "https://subql.green.taurus.subspace.network/v1/graphql",
-      documents: ["./src/**/**.gql"],
+      documents: ["./src/gql/indexer/queries/**.gql"],
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
+    },
+
+    "./src/gql/subgraph/graphql.tsx": {
+      schema:
+        "https://subgraph.satsuma-prod.com/09f017a8af66/marcaurele--322790/mark-stream/api",
+      documents: ["./src/gql/subgraph/queries/**.gql"],
       plugins: [
         "typescript",
         "typescript-operations",
--- web/src/components/ApolloClient.tsx
+++ web/src/components/ApolloClient.tsx
@@ -1,7 +1,8 @@
 "use client";
 // ^ this file needs the "use client" pragma
 
-import { HttpLink } from "@apollo/client";
+import { ApolloLink, HttpLink, split } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
 import {
   ApolloClient,
   ApolloNextAppProvider,
@@ -10,23 +11,37 @@ import {
 
 // have a function to create a client for you
 function makeClient() {
-  const httpLink = new HttpLink({
+  const httpLink1 = new HttpLink({
     // this needs to be an absolute url, as relative urls cannot be used in SSR
     uri: "https://subql.green.taurus.subspace.network/v1/graphql",
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     // fetchOptions: { cache: "no-store" },
-    // you can override the default `fetchOptions` on a per query basis
-    // via the `context` property on the options passed as a second argument
-    // to an Apollo Client data fetching hook, e.g.:
-    // const { data } = useSuspenseQuery(MY_QUERY, { context: { fetchOptions: { cache: "force-cache" }}});
   });
+
+  const httpLink2 = new HttpLink({
+    uri: "https://subgraph.satsuma-prod.com/09f017a8af66/marcaurele--322790/mark-stream/api",
+  });
+
+  // Use split to send data to each link based on a custom condition
+  const link = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === "OperationDefinition" &&
+        definition.operation === "query" &&
+        (definition.name?.value.startsWith("Subgraph") || false)
+      );
+    },
+    httpLink2,
+    httpLink1
+  );
 
   // use the `ApolloClient` from "@apollo/experimental-nextjs-app-support"
   return new ApolloClient({
     // use the `InMemoryCache` from "@apollo/experimental-nextjs-app-support"
     cache: new InMemoryCache(),
-    link: httpLink,
+    link: ApolloLink.from([link]),
   });
 }
 
